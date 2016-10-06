681fdbe75d642a41135af655d56211da9885cc87
DiegoPino <dpino@krayon.cl>
2016-10-06 19:30:25 -0300

    ideas

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -116,7 +116,7 @@
 </nav>
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
+      <span class="lead"><?php print $site_slogan; ?></span>
     <?php endif; ?>
     <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
